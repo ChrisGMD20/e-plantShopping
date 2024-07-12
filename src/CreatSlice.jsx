a3f647da6a1fcd2bebcd028515e1981089e8703f
ChrisGMD20 <gabriel.diyou@gmail.com>
2024-07-12 18:17:44 +0100

    commit

--- src/CreatSlice.jsx
+++ src/CreatSlice.jsx
@@ -1,18 +1,42 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CreatSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
+    cartCount: 0,
+    cartTotalAmount: 0,
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      state.cartCount++;
+      state.cartTotalAmount += action.payload.cost;
+      const existingItem =
+        state.items.length &&
+        state.items.find((item) => item.name === action.payload.name);
+      if (existingItem) {
+        existingItem.quantity++;
+        existingItem.total += action.payload.cost;
+      } else {
+        state.items.push(action.payload);
+      }
     },
     removeItem: (state, action) => {
+      const existingItem =
+        state.items.length &&
+        state.items.find((item) => item.name === action.payload);
+      state.cartCount -= existingItem.quantity;
+      state.cartTotalAmount -= existingItem.total;
+      state.items = state.items.filter((item) => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-    
+      state.cartCount += action.payload.quantity;
+      const existingItem =
+        state.items.length &&
+        state.items.find((item) => item.name === action.payload.name);
+      state.cartTotalAmount += action.payload.quantity * existingItem.cost;
+      existingItem.total += existingItem.cost * action.payload.quantity;
+      existingItem.quantity += action.payload.quantity;
     },
   },
 });
